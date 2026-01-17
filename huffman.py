e1f4adab6c52e48605e6b4ddab8ddc7c1ff3b674
irlxrd <iremssahin.2002@gmail.com>
2026-01-17 23:14:00 +0200

    Huffman encoding added

---
+++ huffman.py
@@ -0,0 +1,136 @@
+import heapq
+import os
+
+
+class HuffmanCoding:
+	def __init__(self, path):
+		self.path = path
+		self.heap = []
+		self.codes = {}
+		self.reverse_mapping = {}
+
+	class HeapNode:
+		def __init__(self, char, freq):
+			self.char = char
+			self.freq = freq
+			self.left = None
+			self.right = None
+
+		#defining comparators less_than and equals
+		def __lt__(self, other):
+			return self.freq < other.freq
+
+		def __eq__(self, other):
+			if(other == None):
+				return False
+			if(not isinstance(other, HeapNode)):
+				return False
+			return self.freq == other.freq
+
+	#functions for compression:
+
+	def make_frequency_dict(self, text):
+    #calculate frequency of each character in the text and return as a dict
+		frequency = {}
+		for character in text:
+			if not character in frequency:
+				frequency[character] = 0
+			frequency[character] += 1
+		return frequency
+
+	def make_heap(self, frequency):
+    #make priority queue from frequency dict
+		for key in frequency:
+			node = self.HeapNode(key, frequency[key])
+			heapq.heappush(self.heap, node) #adds each HeapNode instance to the heap as root
+
+	def merge_nodes(self):
+    #build huffman tree and save root node in heap
+		while(len(self.heap)>1):
+			node1 = heapq.heappop(self.heap)
+			node2 = heapq.heappop(self.heap)
+
+			merged = self.HeapNode(None, node1.freq + node2.freq)
+			merged.left = node1
+			merged.right = node2
+
+			heapq.heappush(self.heap, merged) 
+
+
+	def make_codes_helper(self, root, current_code):
+    #current code is empty string at first call
+		if(root == None):
+			return
+
+		if(root.char != None):
+			self.codes[root.char] = current_code
+			self.reverse_mapping[current_code] = root.char
+			return
+
+        #recursively traverse left and right
+		self.make_codes_helper(root.left, current_code + "0")
+		self.make_codes_helper(root.right, current_code + "1")
+
+
+	def make_codes(self):
+    #make codes for characters
+		root = heapq.heappop(self.heap)
+		current_code = ""
+		self.make_codes_helper(root, current_code)
+
+
+	def get_encoded_text(self, text):
+    #replace each character with its code and return
+		encoded_text = ""
+		for character in text:
+			encoded_text += self.codes[character]
+		return encoded_text
+
+
+	def pad_encoded_text(self, encoded_text):
+    #pad encoded text to make its length a multiple of 8
+		extra_padding = 8 - len(encoded_text) % 8
+		for i in range(extra_padding):
+			encoded_text += "0"
+
+		padded_info = "{0:08b}".format(extra_padding)
+		encoded_text = padded_info + encoded_text
+		return encoded_text
+
+
+	def get_byte_array(self, padded_encoded_text):
+    #convert bits into bytes and return byte array
+		if(len(padded_encoded_text) % 8 != 0):
+			print("Encoded text not padded properly")
+			exit(0)
+
+		b = bytearray()
+		for i in range(0, len(padded_encoded_text), 8):
+			byte = padded_encoded_text[i:i+8]
+			b.append(int(byte, 2))
+		return b
+
+
+	def compress(self):
+		filename, file_extension = os.path.splitext(self.path)
+		output_path = filename + ".bin"
+
+		with open(self.path, 'r+') as file, open(output_path, 'wb') as output:
+			text = file.read()
+			text = text.rstrip()
+
+			frequency = self.make_frequency_dict(text)
+			self.make_heap(frequency)
+			self.merge_nodes()
+			self.make_codes()
+
+			encoded_text = self.get_encoded_text(text)
+			padded_encoded_text = self.pad_encoded_text(encoded_text)
+
+			b = self.get_byte_array(padded_encoded_text)
+			output.write(bytes(b))
+
+		print("Compressed")
+		return output_path
+
+
